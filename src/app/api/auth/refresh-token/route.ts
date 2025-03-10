68ee01ece23a61b9e858ea7eff44147bc42bdbfc
ductaip <dtai858@gmail.com>
2025-03-10 23:25:49 +0700

    done refresh token handler method

--- src/app/api/auth/refresh-token/route.ts
+++ src/app/api/auth/refresh-token/route.ts
@@ -6,26 +6,33 @@ import { HttpError } from "@/lib/http"
 
  
 export async function POST(request: Request) { 
-    const body = (await request.json()) as LoginBodyType
     const cookieStore = await cookies()
-    try { 
-        const {payload} = await authApi.sLogin(body)
-        const {data: {
-            accessToken, refreshToken
-        }} = payload
-        
-        const decodeAccessToken = jwt.decode(accessToken) as {exp: number}
-        const decodeRefreshToken = jwt.decode(refreshToken) as {exp: number}
+    const refreshToken = cookieStore.get('refreshToken')?.value
 
-        cookieStore.set('accessToken', accessToken, {
+    if(!refreshToken) {
+        return Response.json({
+            message: 'Không tìm thấy refreshToken'
+        }, {
+            status: 401
+        })
+    }
+    try { 
+        const {payload} = await authApi.sRefreshToken({refreshToken})
+        
+        //Nhận acc và refreshToken mới thì set mã hóa lại ra..
+        const decodeAccessToken = jwt.decode(payload.data.accessToken) as {exp: number}
+        const decodeRefreshToken = jwt.decode(payload.data.refreshToken) as {exp: number}
+
+        //set cái mới vào cookie
+        cookieStore.set('accessToken', payload.data.accessToken, {
             path: '/',
             httpOnly: true,
             secure: true,
             sameSite: 'lax',
             expires: decodeAccessToken.exp * 1000
         })
-
-        cookieStore.set('refreshToken', refreshToken, {
+        //set mới vào cookie
+        cookieStore.set('refreshToken', payload.data.refreshToken, {
             path: '/',
             httpOnly: true,
             secure: true,
@@ -34,14 +41,8 @@ export async function POST(request: Request) {
         })
 
         return Response.json(payload)
-    } catch (error) {
-        if(error instanceof HttpError) {
-            return Response.json(error.payload, {
-                status: error.status
-            })
-        } else {
-            return Response.json({message: `Có lỗi xảy ra khi login`}, {status: 500})
-        }
+    } catch (error: any) { 
+        return Response.json({message: error.message ?? `Có lỗi xảy ra khi login`}, {status: 401})
     }
    
 }
