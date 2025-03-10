39ddbdd136a537dccdcc3f02f21454140754668a
ductaip <dtai858@gmail.com>
2025-03-10 14:42:29 +0700

    adjust middleware

--- src/app/(public)/(auth)/logout/page.tsx
+++ src/app/(public)/(auth)/logout/page.tsx
@@ -1,15 +1,18 @@
 'use client'
 
+import { getRefreshTokenFromLocalStorage } from "@/lib/utils"
 import { useLogoutMutation } from "@/queries/useAuth"
-import { useRouter } from "next/navigation"
+import { useRouter, useSearchParams } from "next/navigation"
 import { useEffect, useRef } from "react"
 
 export default function LogoutPage() {
+    const searchParams = useSearchParams()
+    const refreshTokenFromUrl = searchParams.get('refreshToken')
     const {mutateAsync} = useLogoutMutation()
     const router = useRouter()
     const ref = useRef<any>(null)
     useEffect(() => {
-        if(ref.current) return
+        if(ref.current || refreshTokenFromUrl !== getRefreshTokenFromLocalStorage() || !refreshTokenFromUrl) return
         ref.current = mutateAsync
         mutateAsync().then((res) => {
             setTimeout(() => {
--- src/middleware.ts
+++ src/middleware.ts
@@ -8,14 +8,23 @@ const unAuthPaths = ['/login']
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const {pathname} = request.nextUrl
-  const isAuth = Boolean(request.cookies.get('accessToken')?.value)
+  const accessToken = request.cookies.get('accessToken')?.value
+  const refreshToken = request.cookies.get('refreshToken')?.value
 
+  if(privatePaths.some((path) => pathname.startsWith(path) && !refreshToken)) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
 
-  if(privatePaths.some((path) => pathname.startsWith(path) && !isAuth))
-    return Response.redirect(new URL('/logout', request.url))
-
-  if(unAuthPaths.some((path) => pathname.startsWith(path) && isAuth))
+  if(unAuthPaths.some((path) => pathname.startsWith(path) && refreshToken))
     return Response.redirect(new URL('/', request.url))
+
+  if(privatePaths.some((path) => pathname.startsWith(path) && !accessToken && refreshToken)) {
+    const url = new URL('/logout', request.url)
+    url.searchParams.set('refreshToken', refreshToken as string)
+    return Response.redirect(url)
+  }
+    
+
 
 
   return NextResponse.next()
