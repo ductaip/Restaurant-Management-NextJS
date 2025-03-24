792579ee318aeadaf64c53bc67c11986d7bdec60
ductaip <dtai858@gmail.com>
2025-03-24 20:54:03 +0700

    fix app provider

--- src/components/app-provider.tsx
+++ src/components/app-provider.tsx
@@ -1,58 +1,61 @@
-'use client'
-import { 
-    QueryClient,
-    QueryClientProvider,
-} from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import RefreshToken from './refresh-token'
-import { createContext, useContext, useEffect, useState } from 'react'
-import { getAccessTokenFromLocalStorage, removeTokensFromLocalStorage } from '@/lib/utils'
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import RefreshToken from "./refresh-token";
+import { createContext, useContext, useEffect, useState } from "react";
+import {
+  decodeToken,
+  getAccessTokenFromLocalStorage,
+  removeTokensFromLocalStorage,
+} from "@/lib/utils";
+import { RoleType } from "@/types/jwt.types";
 
-  // Create a client
+// Create a client
 export const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            refetchOnWindowFocus: false,
-            refetchOnMount: false
-        },
-      },
-})
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+    },
+  },
+});
 
 const AppContext = createContext({
-    isAuth: false,
-    handleSetIsAuth: (isAuth: boolean) => {}
-})
+  role: undefined as RoleType | undefined,
+  setRole: (role?: RoleType | undefined) => {},
+});
 
 export const useAppContext = () => {
-    return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export default function AppProvider({ children }: {
-    children: React.ReactNode
+export default function AppProvider({
+  children,
+}: {
+  children: React.ReactNode;
 }) {
+  const [role, setRoleState] = useState<RoleType | undefined>(undefined);
 
-    const [isAuth, setIsAuth] = useState(false)
-
-    useEffect(() => {
-        const accessToken = getAccessTokenFromLocalStorage()
-        if(accessToken) setIsAuth(true)
-    }, [])
-
-    const handleSetIsAuth = (isAuth: boolean) => {
-        if(isAuth) setIsAuth(true)
-        else {
-            setIsAuth(false)
-            removeTokensFromLocalStorage()
-        }
+  useEffect(() => {
+    const accessToken = getAccessTokenFromLocalStorage();
+    if (accessToken) {
+      const role = decodeToken(accessToken).role;
+      setRoleState(role);
     }
-    //react19 and nextjs 15 don't need AppContext.Provider
-    return (
-      <AppContext value={{isAuth, handleSetIsAuth}}>
-        <QueryClientProvider client={queryClient}>
-            {children}
-            <RefreshToken />
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </AppContext>
-    )
+  }, []);
+
+  const setRole = (role?: RoleType | undefined) => {
+    setRoleState(role);
+    if (!role) removeTokensFromLocalStorage();
+  };
+  //react19 and nextjs 15 don't need AppContext.Provider
+  return (
+    <AppContext value={{ role, setRole }}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <RefreshToken />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </AppContext>
+  );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,140 +1,161 @@
-import { clsx, type ClassValue } from "clsx"
-import { UseFormSetError } from "react-hook-form"
-import { twMerge } from "tailwind-merge"
-import { EntityError } from "./http"
-import { toast } from "sonner"
-import jwt from 'jsonwebtoken'
-import authApi from "@/apis/auth"
-import { DishStatus, OrderStatus, TableStatus } from "@/constants/type"
-import envConfig from "@/config"
+import { clsx, type ClassValue } from "clsx";
+import { UseFormSetError } from "react-hook-form";
+import { twMerge } from "tailwind-merge";
+import { EntityError } from "./http";
+import { toast } from "sonner";
+import jwt from "jsonwebtoken";
+import authApi from "@/apis/auth";
+import { DishStatus, OrderStatus, TableStatus } from "@/constants/type";
+import envConfig from "@/config";
+import { TokenPayload } from "@/types/jwt.types";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export const handleErrorApi = ({error, setError} : {
-  error: any
-  setError?: UseFormSetError<any>
+export const handleErrorApi = ({
+  error,
+  setError,
+}: {
+  error: any;
+  setError?: UseFormSetError<any>;
 }) => {
   if (error instanceof EntityError && setError) {
-    error.payload.errors.forEach(item => {
+    error.payload.errors.forEach((item) => {
       setError(item.field, {
-        type: 'server',
-        message: item.message
-      })
-    })
+        type: "server",
+        message: item.message,
+      });
+    });
   } else {
-    toast(error?.payload?.message ?? 'Error is not known')
+    toast(error?.payload?.message ?? "Error is not known");
   }
-}
+};
 
-const isBrowser = typeof window !== 'undefined'
+const isBrowser = typeof window !== "undefined";
 
-export const getAccessTokenFromLocalStorage = () => isBrowser ? localStorage.getItem('accessToken') : null
- 
+export const getAccessTokenFromLocalStorage = () =>
+  isBrowser ? localStorage.getItem("accessToken") : null;
 
-export const getRefreshTokenFromLocalStorage = () =>  isBrowser ? localStorage.getItem('refreshToken') : null
- 
-export const setAccessTokenToLocalStorage = (value: string) => isBrowser ? localStorage.setItem('accessToken', value) : null
+export const getRefreshTokenFromLocalStorage = () =>
+  isBrowser ? localStorage.getItem("refreshToken") : null;
 
-export const setRefreshTokenToLocalStorage = (value: string) => isBrowser ? localStorage.setItem('refreshToken', value) : null
+export const setAccessTokenToLocalStorage = (value: string) =>
+  isBrowser ? localStorage.setItem("accessToken", value) : null;
+
+export const setRefreshTokenToLocalStorage = (value: string) =>
+  isBrowser ? localStorage.setItem("refreshToken", value) : null;
 
 export const removeTokensFromLocalStorage = () => {
-  isBrowser && localStorage.removeItem('accessToken') 
-  isBrowser && localStorage.removeItem('refreshToken')
-}
+  isBrowser && localStorage.removeItem("accessToken");
+  isBrowser && localStorage.removeItem("refreshToken");
+};
 
-export const checkAndRefreshToken = async (param?:{
-  onError?: () => void
-  onSuccess?: () => void
+export const checkAndRefreshToken = async (param?: {
+  onError?: () => void;
+  onSuccess?: () => void;
 }) => {
-  //check xem token còn hạn ko, 
+  //check xem token còn hạn ko,
   // ko nên đưa ra khỏi hàm.
-  const accessToken = getAccessTokenFromLocalStorage()
-  const refreshToken = getRefreshTokenFromLocalStorage()
-  if(!accessToken || !refreshToken) return
-  const decodeAccessToken = jwt.decode(accessToken) as {
-      exp: number
-      iat: number
-  }
-  const decodeRefreshToken = jwt.decode(refreshToken) as {
-      exp: number
-      iat: number
-  }
+  const accessToken = getAccessTokenFromLocalStorage();
+  const refreshToken = getRefreshTokenFromLocalStorage();
+  if (!accessToken || !refreshToken) return;
+
+  const decodeAccessToken = decodeToken(accessToken);
+
+  const decodeRefreshToken = decodeToken(refreshToken);
   //exp của token tính theo giây, new Date().getTime() thì ms
   //set cookie bi lech, vi the tru hao 1s
-  const now = (new Date().getTime() / 1000 ) - 1
-
+  const now = new Date().getTime() / 1000 - 1;
 
   //refreshToken is expire then logout
-  if(decodeRefreshToken.exp <= now) {
-    removeTokensFromLocalStorage()
-    return param?.onError && param.onError()
+  if (decodeRefreshToken.exp <= now) {
+    removeTokensFromLocalStorage();
+    return param?.onError && param.onError();
   }
   // example if access token has expire time that is 1h
   // check if there is 1/3 time (20p) left then refresh-token again
-  // **leftTime = decodeAccessToken.exp - now 
+  // **leftTime = decodeAccessToken.exp - now
   // **expTime = decodeAccessToken.exp - decodeAccessToken.iat
-  if(decodeAccessToken.exp - now < (decodeAccessToken.exp - decodeAccessToken.iat) / 3) {
-      //call refreshtoken api
-      try {
-          const result = await authApi.refreshToken()
-          console.log('####',result.payload.data.accessToken)
-          setAccessTokenToLocalStorage(result.payload.data.accessToken)
-          setRefreshTokenToLocalStorage(result.payload.data.refreshToken)
-          param?.onSuccess && param.onSuccess()
-      } catch (error) {
-          param?.onError && param.onError()
-      }
-  } 
-}
-
+  if (
+    decodeAccessToken.exp - now <
+    (decodeAccessToken.exp - decodeAccessToken.iat) / 3
+  ) {
+    //call refreshtoken api
+    try {
+      const result = await authApi.refreshToken();
+      console.log("####", result.payload.data.accessToken);
+      setAccessTokenToLocalStorage(result.payload.data.accessToken);
+      setRefreshTokenToLocalStorage(result.payload.data.refreshToken);
+      param?.onSuccess && param.onSuccess();
+    } catch (error) {
+      param?.onError && param.onError();
+    }
+  }
+};
 
 export const formatCurrency = (number: number) => {
-  return new Intl.NumberFormat('vi-VN', {
-    style: 'currency',
-    currency: 'VND'
-  }).format(number)
-}
+  return new Intl.NumberFormat("vi-VN", {
+    style: "currency",
+    currency: "VND",
+  }).format(number);
+};
 
-export const getVietnameseDishStatus = (status: (typeof DishStatus)[keyof typeof DishStatus]) => {
+export const getVietnameseDishStatus = (
+  status: (typeof DishStatus)[keyof typeof DishStatus]
+) => {
   switch (status) {
     case DishStatus.Available:
-      return 'Có sẵn'
+      return "Có sẵn";
     case DishStatus.Unavailable:
-      return 'Không có sẵn'
+      return "Không có sẵn";
     default:
-      return 'Ẩn'
+      return "Ẩn";
   }
-}
+};
 
-export const getVietnameseOrderStatus = (status: (typeof OrderStatus)[keyof typeof OrderStatus]) => {
+export const getVietnameseOrderStatus = (
+  status: (typeof OrderStatus)[keyof typeof OrderStatus]
+) => {
   switch (status) {
     case OrderStatus.Delivered:
-      return 'Đã phục vụ'
+      return "Đã phục vụ";
     case OrderStatus.Paid:
-      return 'Đã thanh toán'
+      return "Đã thanh toán";
     case OrderStatus.Pending:
-      return 'Chờ xử lý'
+      return "Chờ xử lý";
     case OrderStatus.Processing:
-      return 'Đang nấu'
+      return "Đang nấu";
     default:
-      return 'Từ chối'
+      return "Từ chối";
   }
-}
+};
 
-export const getVietnameseTableStatus = (status: (typeof TableStatus)[keyof typeof TableStatus]) => {
+export const getVietnameseTableStatus = (
+  status: (typeof TableStatus)[keyof typeof TableStatus]
+) => {
   switch (status) {
     case TableStatus.Available:
-      return 'Có sẵn'
+      return "Có sẵn";
     case TableStatus.Reserved:
-      return 'Đã đặt'
+      return "Đã đặt";
     default:
-      return 'Ẩn'
+      return "Ẩn";
   }
-}
+};
 
-export const getTableLink = ({ token, tableNumber }: { token: string; tableNumber: number }) => {
-  return envConfig.NEXT_PUBLIC_URL + '/tables/' + tableNumber + '?token=' + token
-}
+export const getTableLink = ({
+  token,
+  tableNumber,
+}: {
+  token: string;
+  tableNumber: number;
+}) => {
+  return (
+    envConfig.NEXT_PUBLIC_URL + "/tables/" + tableNumber + "?token=" + token
+  );
+};
 
+export const decodeToken = (token: string) => {
+  return jwt.decode(token) as TokenPayload;
+};
