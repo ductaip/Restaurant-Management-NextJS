19b42cf07821d5f159381686b9f8928f4e068458
ductaip <dtai858@gmail.com>
2025-03-25 22:24:53 +0700

    middleware: check role and adjust middleware for authorization

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,11 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { decodeToken } from "./lib/utils";
+import { Role } from "./constants/type";
 
-const privatePaths = ["/manage"];
-
+const managePaths = ["/manage"];
+const guestPaths = ["/guest"];
+const privatePaths = [...managePaths, ...guestPaths];
 const unAuthPaths = ["/login"];
 
 // This function can be marked `async` if using `await` inside
@@ -17,24 +20,39 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(url);
   }
 
-  if (unAuthPaths.some((path) => pathname.startsWith(path) && refreshToken))
-    return Response.redirect(new URL("/", request.url));
+  //truong hop da dang nhap
+  if (refreshToken) {
+    //1. Co tinh vao login(unAuthPats) thi se redirect ve trang chu
+    if (unAuthPaths.some((path) => pathname.startsWith(path)))
+      return NextResponse.redirect(new URL("/", request.url));
 
-  //chỉ check xem có acc hay ref_ hay ko chứ ko check có đúng ở server ko
-  if (
-    privatePaths.some(
-      (path) => pathname.startsWith(path) && !accessToken && refreshToken
-    )
-  ) {
-    const url = new URL("/refresh-token", request.url);
-    url.searchParams.set("refreshToken", refreshToken as string);
-    url.searchParams.set("redirect", pathname);
-    return Response.redirect(url);
+    //chỉ check xem có acc hay ref_ hay ko chứ ko check có đúng ở server ko
+    //2. accessToken het han
+    if (
+      privatePaths.some((path) => pathname.startsWith(path) && !accessToken)
+    ) {
+      const url = new URL("/refresh-token", request.url);
+      url.searchParams.set("refreshToken", refreshToken as string);
+      url.searchParams.set("redirect", pathname);
+      return NextResponse.redirect(url);
+    }
+
+    //3. vao ko dung role, redirect ve trang chu
+    const role = decodeToken(refreshToken).role;
+    if (
+      (role === Role.Guest &&
+        managePaths.some((path) => pathname.startsWith(path))) ||
+      (role !== Role.Guest &&
+        guestPaths.some((path) => pathname.startsWith(path)))
+    ) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
   }
+
   return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/login", "/manage/:path*"],
+  matcher: ["/login", "/manage/:path*", "/guest/:path*"],
 };
